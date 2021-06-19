3c8e48cfca0afde43fcee9315e58a97f6b95b2ac
Michaella Magtibay <michaeltmagtibay@gmail.com>
2021-06-19 23:47:05 +0700

    change css > scss and add placeholder names on views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+
+import Header from "./components/Header";
+
+import Home from "./views/Home";
+import About from "./views/About";
+import Work from "./views/Work";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <div>
+          <Route path exact="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/work" component={Work} />
+        </div>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+
+import Header from "./components/Header";
+
+import Home from "./views/Home";
+import About from "./views/About";
+import Work from "./views/Work";
+
+const routing = (
+  <Router>
+    <div>
+      <Header />
+      <Route path exact="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/work" component={Work} />
+    </div>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
